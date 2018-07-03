6f70600f372316101c4adcded73540f947d02195
vtlongbeo <vt.longbeo@gmail.com>
2018-07-03 10:47:43 +0700

    ok

---
+++ TinhLuong/Controllers/ImportDonDichCapDongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinhLuong.Controllers
+{
+    public class ImportDonDichCapDongController : Controller
+    {
+        // GET: ImportDonDichCapDong
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
